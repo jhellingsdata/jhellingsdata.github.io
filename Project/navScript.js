f26ed0dc430f16ed640b03f898b1f63077e9ff3e
jhellingsdata <91467803+jhellingsdata@users.noreply.github.com>
2022-01-11 00:33:28 +0000

    Create project folder

---
+++ Project/navScript.js
@@ -0,0 +1,7 @@
+/* --- js for interactive navbar ---- */ 
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
